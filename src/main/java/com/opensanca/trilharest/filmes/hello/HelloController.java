8a61b2a95db7769f7c16e227f027f9e41b6afdf4
arirneto <ari.neto.ribeiro@gmail.com>
2017-10-24 13:51:22 -0200

    Atualiza Ola

--- src/main/java/com/opensanca/trilharest/filmes/hello/HelloController.java
+++ src/main/java/com/opensanca/trilharest/filmes/hello/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
 
     @RequestMapping(path="", method={ RequestMethod.GET })
     public String hello() {
-        return "Diego é um frufru kkkkkk!";
+        return "Olá!";
     }
 
 }
